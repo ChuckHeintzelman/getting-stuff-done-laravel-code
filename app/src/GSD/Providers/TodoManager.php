8a6921bfccce15db2f81d7ca4c7310084d8d656c
Chuck Heintzelman <chuckh@gmail.com>
2013-09-21 12:19:26 -0700

    Added makeList()

--- app/src/GSD/Providers/TodoManager.php
+++ app/src/GSD/Providers/TodoManager.php
@@ -1,5 +1,7 @@
 <?php namespace GSD\Providers;
 
+use App;
+
 class TodoManager {
 
     /**
@@ -8,5 +10,26 @@ class TodoManager {
     public function imATeapot()
     {
         return "I'm a teapot.";
+    }
+
+    /**
+     * Create a new TODO List
+     * @param string $id The basename of the list
+     * @param string $title The title of the list
+     * @return ListInterface The newly created list
+     * @throws InvalidArgumentException If the list already exists
+     */
+    public function makeList($id, $title)
+    {
+        $repository = App::make('TodoRepositoryInterface');
+        if ($repository->exists($id))
+        {
+            throw new \InvalidArgumentException("A list with id=$id already exists");
+        }
+        $list = App::make('ListInterface');
+        $list->set('id', $id)
+             ->set('title', $title)
+             ->save();
+        return $list;
     }
 }
