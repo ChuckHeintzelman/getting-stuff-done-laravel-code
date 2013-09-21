2bf3d742cafecfa559457f6631e7fee7a0723ebc
Chuck Heintzelman <chuckh@gmail.com>
2013-09-21 16:19:04 -0700

    Added  to methods

--- app/src/GSD/Repositories/TodoRepositoryInterface.php
+++ app/src/GSD/Repositories/TodoRepositoryInterface.php
@@ -1,30 +1,38 @@
 <?php namespace GSD\Repositories;
 
-// File: app/src/GSD/Repositories/TodoRepositoryInterface.php
-
 use GSD\Entities\ListInterface;
 
 interface TodoRepositoryInterface {
 
     /**
      * Does the todo list exist?
-     * @param string $id ID of the list
+     * @param  string $id ID of the list
+     * @param  boolean $archived Check for archived lists only?
      * @return boolean
      */
-    public function exists($id);
+    public function exists($id, $archived = false);
 
     /**
-     * Load a TodoList form it's id
-     * @param  string $id ID of the list
-     * @return ListInterface The List
-     * @throws  InvalidArgumentException If $id not found
+     * Return the ids of all the lists
+     * @param  boolean $archived Return archived ids or unarchived?
+     * @return array of list ids
      */
-    public function load($id);
+    public function getAll($archived = false);
+
+    /**
+     * Load a TodoList from it's id
+     * @param string $id ID of the list
+     * @param boolean $archived Load an archived list?
+     * @return ListInterface The list
+     * @throws InvalidArgumentException If $id not found
+     */
+    public function load($id, $archived = false);
 
     /**
      * Save a TodoList
      * @param string $id ID of the list
+     * @param boolean $archived Save an archived list?
      * @param ListInterface $list The TODO List
      */
-    public function save($id, ListInterface $list);
+    public function save($id, ListInterface $list, $archived = false);
 }
