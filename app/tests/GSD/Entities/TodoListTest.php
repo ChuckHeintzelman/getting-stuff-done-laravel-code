87a304f96b705ba887398d96c64d50aee7a9c3ee
Chuck Heintzelman <chuckh@gmail.com>
2013-09-21 21:45:50 -0700

    Finished unit tests

--- app/tests/GSD/Entities/TodoListTest.php
+++ app/tests/GSD/Entities/TodoListTest.php
@@ -96,4 +96,39 @@ class TodoListTest extends TestCase {
         $result = $list->save();
         $this->assertSame($list, $result);
     }
+
+    public function testArchiveWhenAlreadyArchivedDoesNothing()
+    {
+        $list = App::make('GSD\Entities\TodoList');
+        $list->set('archived', true);
+        $list->archive();
+    }
+
+    /**
+     * @expectedException RuntimeException
+     * @expectedExceptionMessage Cannot archive if id not set
+     */
+    public function testArchiveWithNoIdThrowsException()
+    {
+        $list = App::make('GSD\Entities\TodoList');
+        $list->archive();
+    }
+
+    /**
+     * @expectedException RuntimeException
+     * @expectedExceptionMessage Repository failed deleting unarchived list
+     */
+    public function testArchiveWhenRepoFailsOnDelete()
+    {
+        App::bind('GSD\Repositories\TodoRepositoryInterface', function()
+        {
+            $mock = Mockery::mock('GSD\Repositories\TodoRepositoryInterface');
+            $mock->shouldReceive('exists')->once()->andReturn(true);
+            $mock->shouldReceive('delete')->once()->andReturn(false);
+            return $mock;
+        });
+        $list = App::make('GSD\Entities\TodoList');
+        $list->set('id', 'actions');
+        $list->archive();
+    }
 }
